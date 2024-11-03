893e2a36898cd7b0895ff976500e4dd14d879a82
Marvin Haase <marvin.sempa.haase@gmail.com>
2024-11-03 02:28:21 +0100

    Add g alias

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -5,4 +5,5 @@ alias ccat='pymentize -g -O style="material,lineos=1"'
 alias dckr='docker run --rm -it --entrypoint /bin/sh'
 alias dckrmnt'dckr -v .:/mnt -w /mnt'
 alias vim='nvim'
+alias g='git'
 
