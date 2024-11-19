e02144b1d4ab3a257dcb8d001b185d8d3f40b36f
Marfien <contact@marfien.dev>
2024-11-19 13:22:31 +0100

    Fixed typo

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 alias zshconfig='vim ~/.zshrc'
-alias ccat='pymentize -g -O style="material,lineos=1"'
+alias ccat='pygmentize -g -O style="material,lineos=1"'
 alias dckr='docker run --rm -it --entrypoint /bin/sh'
 alias dckrmnt'dckr -v .:/mnt -w /mnt'
 alias vim='nvim'
