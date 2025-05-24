ed3d5e95c112e2890501cca0a57b6fd1829887c5
Marvin Haase <contact@marfien.dev>
2025-05-24 22:18:39 +0200

    fix(zsh): fzf config

--- config/zsh/plugins/fzf.plugin.zsh
+++ config/zsh/plugins/fzf.plugin.zsh
@@ -1,4 +1,10 @@
 export FZF_CTRL_T_OPTS="--walker-skip .git,node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-export FZF_DEFAULT_OPTS="--style full --preview 'bat -n --color-always {}' --bind 'enter:become(vim {})'"
+export FZF_DEFAULT_OPTS="--style full"
+alias fzf="fzf --preview 'bat -n --color=always {}'"
 
-$(brew --prefix)/opt/fzf/install
+fzf_path_addition="$(brew --prefix)/opt/fzf/bin"
+if [[ ! "$PATH" == *$fzf_path_addition* ]]; then
+  PATH="${PATH:+${PATH}:}$fzf_path_addition"
+fi
+
+source <(fzf --zsh)
