417aa6a1fe28ae9232423d9189f44ea6f7045be2
Marvin Haase <contact@marfien.dev>
2025-05-25 12:13:20 +0200

    feat(nvim): latex compile output dir

--- config/nvim/lua/plugins/language-support.lua
+++ config/nvim/lua/plugins/language-support.lua
@@ -49,6 +49,11 @@ return {
 
       vim.g.vimtex_quickfix_open_on_warning = 0
       vim.g.vimtex_complete_enabled = 1
+
+      vim.g.vimtex_compiler_latexmk = {
+        aux_dir = "build/aux/",
+        out_dir = "build/out/",
+      }
     end,
   },
 }
