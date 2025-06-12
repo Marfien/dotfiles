905a7d29ac6f092f50ed91e67316db37b5a92d17
Marvin Haase <contact@marfien.dev>
2025-06-12 20:36:18 +0200

    feat(nvim): enhance compatibility when editing tex on wsl

--- config/nvim/lua/plugins/lang.tex.lua
+++ config/nvim/lua/plugins/lang.tex.lua
@@ -3,7 +3,12 @@ return {
     "lervag/vimtex",
     lazy = false, -- lazy-loading will disable inverse search
     config = function()
+      -- configure viewer
       vim.g.vimtex_view_method = "sioyek"
+      if vim.env.WSL_DISTRO_NAME then
+        -- Windows path is appended so we will use the windows version
+        vim.g.vimtex_view_sioyek_exe = "sioyek.exe"
+      end
 
       vim.g.vimtex_quickfix_open_on_warning = 0
       vim.g.vimtex_complete_enabled = 1
