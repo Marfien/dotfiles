84ce0833d9658e44e3e26ccac5381e5586a2a942
Marvin Haase <contact@marfien.dev>
2025-07-09 14:53:40 +0200

    feat(nvim): recursivly open folders with just one child node

--- config/nvim/lua/plugins/ui.snacks.lua
+++ config/nvim/lua/plugins/ui.snacks.lua
@@ -7,6 +7,64 @@ return {
           hidden = true,
           ignored = true,
           exclude = { ".git" },
+          actions = {
+            confirm_recurivly = function(picker, item)
+              local actions = require("snacks.explorer.actions")
+              local tree = require("snacks.explorer.tree")
+
+              local might_get_child = function(node)
+                local child = nil
+                for _, node_child in pairs(node.children) do
+                  if child then
+                    return nil
+                  end
+                  child = node_child
+                end
+
+                return child
+              end
+
+              local function open_recursive(node)
+                tree:toggle(node.path)
+                actions.update(picker, { refresh = true })
+                vim.schedule(function()
+                  local child = might_get_child(node)
+
+                  if not child or not child.dir then
+                    return
+                  end
+
+                  open_recursive(child)
+                end)
+              end
+
+              local node = tree:node(item.file)
+
+              if not node then
+                return
+              end
+
+              if node.dir then
+                open_recursive(node)
+              else
+                picker:action("confirm")
+              end
+            end,
+          },
+          win = {
+            list = {
+              keys = {
+                ["<CR>"] = { "confirm_recurivly", mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+          exclude = {
+            "^import.*",
+          },
         },
         files = {
           hidden = true,
@@ -14,8 +72,6 @@ return {
           exclude = { ".git", "*.class" },
         },
       },
-      hidden = true,
-      ignored = true,
     },
   },
 }
