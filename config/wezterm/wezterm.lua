ab87d5f28da80e1a523a01e920076e2325cc5124
Marvin Haase <contact@marfien.dev>
2025-08-05 20:56:50 +0200

    feat(wezterm): catppuccin theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,9 +1,17 @@
 local wezterm = require("wezterm")
 local default_distro = "Fedora"
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Frappe"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 local config = {
 	font = wezterm.font("MesloLGL Nerd Font Mono"),
-	color_scheme = "Tokyo Night",
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	hide_tab_bar_if_only_one_tab = true,
 	max_fps = 30,
 	quote_dropped_files = "Posix",
