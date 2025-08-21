9fa93f3ad433e656978adaa506e5434fdb5ede76
Marvin Haase <contact@marfien.dev>
2025-08-21 16:44:56 +0200

    feat(nvim): update keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,71 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local keymap = vim.keymap
+local map = keymap.set
+local unmap = function(mode, lhs, desc)
+  map(mode, lhs, function()
+    vim.notify("Unmapped: " .. desc)
+  end, { desc = desc })
+end
+
+-- disable arrow keys
+unmap({ "n", "v" }, "<Up>", "Arrows are to be shot, not to move!")
+unmap({ "n", "v" }, "<Down>", "Arrows are to be shot, not to move!")
+unmap({ "n", "v" }, "<Left>", "Arrows are to be shot, not to move!")
+unmap({ "n", "v" }, "<Right>", "Arrows are to be shot, not to move!")
+
+-- better up/down movement for wrapped lines
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- move between windows/panes
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize windows
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- buffers
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+-- TODO: delete buffers
+map("n", "<leader>bd", "<cmd>delbuf<cr>", { desc = "Delete current buffer" })
+map("n", "<leader>bo", "<cmd>delbuf<cr>", { desc = "Delete other buffers" })
+map("n", "<leader>bD", "<cmd>delbuf<cr>", { desc = "Delete current buffer and window" })
+
+-- unified search behavior
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- easier indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- Lazy UI
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- Terminal
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- Window management
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+
+-- TODO:
+-- locations
+-- quickfixes
+-- diagnostics
