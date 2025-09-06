654860c4cf43de3eb559e7119826b3f22d62d6f7
Marvin Haase <contact@marfien.dev>
2025-09-06 10:58:11 +0200

    feat(nvim): python support

---
+++ config/nvim/lua/plugins/lang/python.lua
@@ -0,0 +1,29 @@
+return require("util.lsp").ensure_lang({
+  parsers = { "python" },
+  ft = { "py" },
+  lsp = "jedi-language-server",
+  formatters = { "black" },
+  test_adapter = function()
+    require("neotest-python")
+  end,
+  dap = "debugpy",
+  other = {
+    {
+      "mfussenegger/nvim-dap-python",
+      ft = "py",
+      config = function()
+        require("dap-python").setup("uv")
+      end,
+    },
+    {
+      "nvim-neotest/neotest-python",
+      ft = "py",
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+      },
+      opst = {
+        python = nil,
+      },
+    },
+  },
+})
