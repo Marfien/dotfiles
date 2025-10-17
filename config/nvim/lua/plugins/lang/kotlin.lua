bf5a73206b096bfbd4ee87c7c574c94a0197a7e1
Marvin Haase <contact@marfien.dev>
2025-10-17 10:05:16 +0200

    chore(nvim): setting env on init (android)

--- config/nvim/lua/plugins/lang/kotlin.lua
+++ config/nvim/lua/plugins/lang/kotlin.lua
@@ -4,11 +4,11 @@ return require("util.lsp").ensure_lang({
   tools = { "ktfmt", "kotlin-lsp" },
   other = {
     {
-      "ariedov/android-nvim",
+      "Marfien/android-nvim",
       cmd = { "AndroidBuildRelease", "AndroidRun", "AndroidClean", "LaunchAvd", "AndroidUninstall" },
       init = function()
         vim.env.ANDROID_SDK_HOME = vim.env.XDG_CONFIG_HOME .. "/.android/"
-        vim.g.android_sdk = vim.env.WSL_DISTRO_NAME and vim.fn.stdpath("config") .. "/assets/android-sdk-wsl/"
+        vim.env.ANDROID_HOME = vim.env.WSL_DISTRO_NAME and vim.fn.stdpath("config") .. "/assets/android-sdk-wsl/"
           or require("util.brew").get_brew_path() .. "/share/android-commandlinetools"
       end,
       opts = {},
