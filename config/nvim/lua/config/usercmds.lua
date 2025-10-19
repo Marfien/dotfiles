09601212f60fbcad3703ac45de5a68bb3fb0ad26
Marvin Haase <contact@marfien.dev>
2025-10-19 19:13:12 +0200

    feat(nvim): Add LspRestart user cmd

--- config/nvim/lua/config/usercmds.lua
+++ config/nvim/lua/config/usercmds.lua
@@ -5,3 +5,27 @@ vim.api.nvim_create_user_command("LspLog", function()
 end, {
   desc = "Opens the Nvim LSP client log.",
 })
+
+vim.api.nvim_create_user_command("LspRestart", function()
+  local clients = vim
+    .iter(vim.lsp.get_clients())
+    :map(function(client)
+      return client.name
+    end)
+    :totable()
+
+  for _, client in ipairs(clients) do
+    vim.lsp.enable(client, false)
+  end
+
+  local timer = assert(vim.uv.new_timer())
+  timer:start(500, 0, function()
+    for _, name in ipairs(clients) do
+      vim.schedule_wrap(function(x)
+        vim.lsp.enable(x)
+      end)(name)
+    end
+  end)
+end, {
+  desc = "Restarts all currently attached LSP clients",
+})
