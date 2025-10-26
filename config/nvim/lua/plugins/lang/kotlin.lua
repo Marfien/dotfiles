f1f5864513d9f9d22cb9b09b492ca0620388cc75
Marvin Haase <contact@marfien.dev>
2025-10-26 12:24:26 +0100

    fix(nvim): add android env

--- config/nvim/lua/plugins/lang/kotlin.lua
+++ config/nvim/lua/plugins/lang/kotlin.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "kotlin",
-  callback = function(data)
+  callback = function()
+    vim.env.ANDROID_USER_HOME = vim.env.XDG_CONFIG_HOME .. "/android/"
+    vim.env.ANDROID_HOME = vim.env.WSL_DISTRO_NAME and vim.fn.stdpath("config") .. "/assets/android-sdk-wsl/"
+      or require("util.brew").get_brew_path() .. "/share/android-commandlinetools"
     require("util.android").setup()
   end,
 })
@@ -10,20 +13,4 @@ return require("util.lsp").ensure_lang({
   parsers = { "kotlin" },
   formatters = { "ktfmt" },
   tools = { "ktfmt", "kotlin-lsp" },
-  other = {
-    {
-      --"Marfien/android-nvim",
-      dir = "~/Workspace/github/marfien/android-nvim/",
-      cmd = { "AndroidBuildRelease", "AndroidRun", "AndroidClean", "LaunchAvd", "AndroidUninstall" },
-      init = function()
-        vim.env.ANDROID_SDK_HOME = vim.env.XDG_CONFIG_HOME
-        vim.env.ANDROID_USER_HOME = vim.env.XDG_CONFIG_HOME .. "/.android/"
-        vim.env.ANDROID_EMULATOR_HOME = vim.env.ANDROID_USER_HOME
-        vim.env.ANDROID_AVD_HOME = vim.env.ANDROID_USER_HOME .. "/avd"
-        vim.env.ANDROID_HOME = vim.env.WSL_DISTRO_NAME and vim.fn.stdpath("config") .. "/assets/android-sdk-wsl/"
-          or require("util.brew").get_brew_path() .. "/share/android-commandlinetools"
-      end,
-      opts = {},
-    },
-  },
 })
