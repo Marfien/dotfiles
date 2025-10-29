542fb63025e82df91bc8b77eb480667f0c843752
Marvin Haase <contact@marfien.dev>
2025-10-29 21:46:52 +0100

    feat(nvim): open dashboard when no buffer available

--- config/nvim/lua/plugins/ui/dashboard.lua
+++ config/nvim/lua/plugins/ui/dashboard.lua
@@ -1,7 +1,21 @@
+local augroup = vim.api.nvim_create_augroup("dashboard", {})
+
 vim.api.nvim_create_autocmd("User", {
+  group = augroup,
   pattern = "BtwOpened",
   callback = function()
     vim.bo.modifiable = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufNew", {
+  group = augroup,
+  callback = function(event)
+    vim.schedule(function()
+      if vim.api.nvim_buf_get_name(event.buf) == "" and vim.bo.filetype == "" then
+        require("btw").open(event.buf)
+      end
+    end)
   end,
 })
 
@@ -31,7 +45,8 @@ return {
   _/\.-'                      _) )    `. \ /                      __/~\/\-.__  
 .'                           (__/       ) )                                  ` 
  
-I use Neovim (BTW)]],
+I use Neovim (BTW)
+]],
     },
   },
 }
