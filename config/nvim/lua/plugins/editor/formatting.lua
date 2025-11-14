4856396ab5a278371ae6574a9e106d0db1144a67
Marvin Haase <contact@marfien.dev>
2025-11-14 18:24:06 +0100

    feat(nvim): scroll left on format

--- config/nvim/lua/plugins/editor/formatting.lua
+++ config/nvim/lua/plugins/editor/formatting.lua
@@ -1,3 +1,17 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ConformFormatPost",
+  callback = function(event)
+    print("CFP")
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == event.buf then
+        vim.api.nvim_win_call(win, function()
+          vim.api.nvim_feedkeys("zH", "n", false)
+        end)
+      end
+    end
+  end,
+})
+
 return {
   {
     "stevearc/conform.nvim",
