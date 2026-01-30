d459b344ea0e1a8ddc8ee77034ffcb0db4d38581
Marvin Haase <contact@marfien.dev>
2026-01-30 12:43:08 +0100

    chore(nvim): adjust keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -70,7 +70,7 @@ map({ "n", "x" }, "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
 map({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 map({ "n", "x" }, "<leader>ck", vim.lsp.buf.signature_help, { desc = "Code Signature" })
 map("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Code Signature" })
-map({ "n", "x" }, "<leader>cD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+map({ "n", "x" }, "gd", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
 map({ "n", "x" }, "K", vim.lsp.buf.hover, { desc = "Hover Documentation" }) -- See `:help K` for why this keymap
 
 -- sessions
--- config/nvim/lua/plugins/ui/terminal.lua
+++ config/nvim/lua/plugins/ui/terminal.lua
@@ -25,7 +25,7 @@ return {
     keys = {
       { "<C-/>", "<cmd>ToggleTerm<cr>", desc = "Toggle Term", mode = { "n", "t" } },
       { "<c-_>", "<cmd>ToggleTerm<cr>", desc = "which_key_ignore", mode = { "n", "t" } },
-      { "<esc><esc><esc>", "<C-\\><C-n>", desc = "Normal Mode", mode = { "t" } },
+      { "<esc><esc>", "<C-\\><C-n>", desc = "Normal Mode", mode = { "t" } },
     },
   },
 }
