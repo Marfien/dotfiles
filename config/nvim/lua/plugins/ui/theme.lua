ea933d0a5f4af23446c2db24ad4330e3401ce055
Marvin Haase <contact@marfien.dev>
2026-02-19 12:16:49 +0100

    chore: adjust theme on line numbers

--- config/nvim/lua/plugins/ui/theme.lua
+++ config/nvim/lua/plugins/ui/theme.lua
@@ -4,7 +4,14 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({})
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        on_highlights = function(hl, c)
+          hl.LineNr = { fg = c.fg_dark }
+          hl.LineNrAbove = hl.LineNr
+          hl.LineNrBelow = hl.LineNr
+        end,
+      })
       vim.cmd.colorscheme("tokyonight")
     end,
   },
