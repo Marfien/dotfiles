9b370a832dad8e499babd4d56f1f7f98a6a9d673
Marvin Haase <contact@marfien.dev>
2026-06-24 09:14:02 +0200

    fix(nvim/gopls): add build flags for integration tests

--- home/default/neovim/nvim/lsp/gopls.lua
+++ home/default/neovim/nvim/lsp/gopls.lua
@@ -89,6 +89,13 @@ end
 return {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  settings = {
+    gopls = {
+      buildFlags = {
+        "-tags=integration"
+      },
+    },
+  },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
     get_mod_cache_dir()
