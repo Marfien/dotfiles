ddfeb353d25ada89f1a2993af89251f62293d68d
Marvin Haase <contact@marfien.dev>
2026-07-18 14:10:24 +0200

    fix(neovim): cannot detect light mode

--- home/mac/neovim.nix
+++ home/mac/neovim.nix
@@ -6,11 +6,8 @@
       ''
         vim.uv.new_timer():start(0, 2000, function()
           vim.system({ "defaults", "read", "-g", "AppleInterfaceStyle" }, { text = true }, function(res)
-            if res.code ~= 0 then
-              vim.notify_once("Could not detect system theme", vim.log.levels.WARN)
-              return
-            end
-
+            -- With light mode the command fails as the entry is not present.
+            -- It's not nice but that's how it works...
             vim.schedule(function()
               vim.api.nvim_set_option_value("background", res.stdout:match("Dark") and "dark" or "light", {})
             end)
